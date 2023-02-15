699844fa769c18a196ba11f809547214466a15a9
Frida Brodin <frida.brodin@hotmail.com>
2023-02-15 16:50:43 +0100

    Changed error handling

--- script/calories.js
+++ script/calories.js
@@ -2,30 +2,44 @@
 const submitButton = document.querySelector("#bmr-submit-button");
 const bmrForm = document.querySelector("#bmr-calculator");
 
-bmrForm.addEventListener("submit", calculateBMR);
+bmrForm.addEventListener("submit", calculateCalories);
 
-
-function calculateBMR(event) {
+function calculateCalories (event) {
     event.preventDefault();
 
-
-    const gender = document.querySelector('input[name="gender"]:checked');
-    const age = document.querySelector("#age").value;
-    const weight = document.querySelector("#weight").value;
-    const height = document.querySelector("#height").value;
     const activity = document.querySelector('input[name="activity"]:checked');
-    let activityRate;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    
+    // Error handling
+
     const errorMessageActivity = document.querySelector(".error-message-container.activity");
     const errorMessageGender = document.querySelector(".error-message-container.gender");
 
+    if (gender == null && activity == null) {
+
+        errorMessageGender.classList.add("OPEN");
+        errorMessageActivity.classList.add("OPEN");
+        return;
+
+    }  else if (gender == null || gender == null & !activity == null) {     
+
+        errorMessageActivity.classList.remove("OPEN");
+        errorMessageGender.classList.add("OPEN");
+        return;
+
+    } else if (activity == null || activity == null && !gender == null) {     
+
+        errorMessageGender.classList.remove("OPEN");
+        errorMessageActivity.classList.add("OPEN");
+        return;
+
+    } 
 
     // Check activity rate
+    
+    let activityRate;
 
-    if (activity == null || activity == null && !gender == null) {
-
-        errorMessageActivity.classList.add("OPEN");
-
-    } else if (activity.value == 1) {
+    if (activity.value == 1) {
 
         errorMessageActivity.classList.remove("OPEN");
         activityRate = 1.2;
@@ -55,47 +69,43 @@ function calculateBMR(event) {
     } 
 
     // Remove old results if there is any
+
     const resultContainer = document.querySelector("#bmr-result");
 
     if (resultContainer.children.length === 1) {      
         resultContainer.children[0].remove(); 
     }
 
-    // Calculate based on gender
+    // Calculate based on gender, weight, age and activity
 
-    if (gender == null || gender == null & !activity == null) {     
+    let genderValue;
 
-        errorMessageGender.classList.add("OPEN");
-        return;
+    if (gender.value == "female") {
 
-    } else if (gender.value == "female") {
-
-        let BMR = 10 * weight + 6.25 * height - 5 * age - 161;
-
-        errorMessageGender.classList.remove("OPEN");
-        resultContainer.classList.add("OPEN");
-        let div = resultContainer.appendChild(document.createElement("div"));
-        let heading = div.appendChild(document.createElement("h2"));
-        let paragraph = div.appendChild(document.createElement("p"));
-
-        heading.innerHTML = "Your BMR";
-        paragraph.innerHTML = "Your BMR is " + BMR * activityRate + " kcal.";
-        
+        genderValue = - 161;     
 
     } else if (gender.value == "male") {
 
-        let BMR = 10 * weight + 6.25 * height - 5 * age + 5 * activityRate;
-
-        errorMessageGender.classList.remove("OPEN");
-        resultContainer.classList.add("OPEN");
-        let div = resultContainer.appendChild(document.createElement("div"));
-        let heading = div.appendChild(document.createElement("h2"));
-        let paragraph = div.appendChild(document.createElement("p"));
-
-        heading.innerHTML = "Your BMR";
-        paragraph.innerHTML = "Your BMR is " + BMR * activityRate + " kcal.";
+        genderValue = + 5;
 
     } 
+
+
+    // Calculate based on gender, weight, age and activity
+
+    const age = document.querySelector("#age").value;
+    const weight = document.querySelector("#weight").value;
+    const height = document.querySelector("#height").value;
+
+    let BMR = 10 * weight + 6.25 * height - 5 * age + genderValue;
+
+    resultContainer.classList.add("OPEN");
+    let div = resultContainer.appendChild(document.createElement("div"));
+    let heading = div.appendChild(document.createElement("h2"));
+    let paragraph = div.appendChild(document.createElement("p"));
+
+    heading.innerHTML = "Calorie intake";
+    paragraph.innerHTML = "Your daily calorie need is " + BMR * activityRate + " kcal.";
 
 
 }
